b7239ec3702fdd0760eafb67f42e4a278e33db4a
Eric Richter <erichte@linux.ibm.com>
2023-05-22 15:07:26 -0500

    test: rewrite tests to reduce output, consist use of assert()
    Some of the test cases outputted way more information than is necessary.
    In the case of CI, we should only see the information we need to debug a
    problem. Furthermore, a test case binary should return a different return
    for successes and failures -- currently some test cases always return 0.
    
    This patch:
     - rewrites the logic to use assert() for all checks
     - removes all unnecessary printfs
     - disables prlog output from the library to limit excess output
    
    This patch also include some helpers like assert_rc() to automate checking
    return codes and print out a helpful message that translates error codes
    to SV_ error strings.
    
    The use of assert handles both the output of what failed, as well as returning
    a nonzero return code, so that a CI or build system can detect a failed case
    and handle that appropriately.
    
    Signed-off-by: Eric Richter <erichte@linux.ibm.com>

--- test/test_auth2.c
+++ test/test_auth2.c
@@ -10,6 +10,7 @@
 #include "data/delete_pk_sesl_auth.h"
 #include "data/svc_dbx_by_KEK_auth.h"
 #include <stdio.h>
+#include "test_utils.h"
 
 __attribute__((unused)) static const uint16_t *DBX_NAME = (const uint16_t *)"d\0b\0x\0\0\0";
 
@@ -23,65 +24,56 @@ main (int argc, char **argv)
   timestamp_t timestamp;
   uint8_t hash[MAX_HASH_SIZE] = { 0 };
 
-  printf ("\n5 test cases for unpacking of authenticated variable\n\n");
+  printf ("testing authenticated variables unpack...");
 
   auth_data.auth_msg = pk_auth;
   auth_data.auth_msg_size = pk_auth_len;
 
   rc = unpack_authenticated_variable (&auth_data, &timestamp, &cert, &cert_size,
                                       &data, &data_size);
-  if (rc == SV_SUCCESS && (cert_size > 0 && cert_size < pk_auth_len) &&
-      data_size == 857 && (data == pk_auth + pk_auth_len - data_size) &&
-      le16_to_cpu (timestamp.year) == 2022)
-    printf ("Test case-1 : PASSED\n");
-  else
-    {
-      printf ("Test case-1 : FAILED with rc = 0x%x\n", rc);
-      return 0;
-    }
+
+  assert_rc (SV_SUCCESS);
+  assert (cert_size > 0);
+  assert (cert_size < pk_auth_len);
+  assert (data_size == 857);
+  assert (data == pk_auth + pk_auth_len - data_size);
+  assert (le16_to_cpu(timestamp.year) == 2022);
 
   auth_data.auth_msg = delete_pk_sbvs_auth;
   auth_data.auth_msg_size = delete_pk_sbvs_auth_len;
 
   rc = unpack_authenticated_variable (&auth_data, &timestamp, &cert, &cert_size,
                                       &data, &data_size);
-  if (rc == SV_SUCCESS && (cert_size > 0 && cert_size < delete_pk_sbvs_auth_len) &&
-      data_size == 0 && (le16_to_cpu (timestamp.year) == 2022 - 1900))
-    printf ("Test case-2 : PASSED\n");
-  else
-    {
-      printf ("Test case-2 : FAILED with rc = 0x%x\n", rc);
-      return 0;
-    }
+
+  assert_rc (SV_SUCCESS);
+  assert (cert_size < delete_pk_sbvs_auth_len);
+  assert (data_size == 0);
+  assert (le16_to_cpu (timestamp.year) == 2022 - 1900);
 
   auth_data.auth_msg = delete_pk_sesl_auth;
   auth_data.auth_msg_size = delete_pk_sesl_auth_len;
 
   rc = unpack_authenticated_variable (&auth_data, &timestamp, &cert, &cert_size,
                                       &data, &data_size);
-  if (rc == SV_SUCCESS && (cert_size > 0 && cert_size < delete_pk_sesl_auth_len) &&
-      data_size == 0 && (le16_to_cpu (timestamp.year) == 2022))
-    printf ("Test case-3 : PASSED\n");
-  else
-    {
-      printf ("Test case-3 : FAILED with rc = 0x%x\n", rc);
-      return 0;
-    }
+
+  assert_rc (SV_SUCCESS);
+  assert (cert_size > 0);
+  assert (cert_size < delete_pk_sesl_auth_len);
+  assert (data_size == 0);
+  assert (le16_to_cpu (timestamp.year) == 2022);
 
   auth_data.auth_msg = svc_dbx_by_KEK_auth;
   auth_data.auth_msg_size = svc_dbx_by_KEK_auth_len;
 
   rc = unpack_authenticated_variable (&auth_data, &timestamp, &cert, &cert_size,
                                       &data, &data_size);
-  if (rc == SV_SUCCESS && (cert_size > 0 && cert_size < svc_dbx_by_KEK_auth_len) &&
-      (data_size > 0 && data_size < svc_dbx_by_KEK_auth_len) &&
-      (le16_to_cpu (timestamp.year) == 2020))
-    printf ("Test case-4 : PASSED\n");
-  else
-    {
-      printf ("Test case-4 : FAILED with rc = 0x%x\n", rc);
-      return 0;
-    }
+
+  assert_rc (SV_SUCCESS);
+  assert (cert_size > 0);
+  assert (cert_size < svc_dbx_by_KEK_auth_len);
+  assert (data_size > 0);
+  assert (data_size < svc_dbx_by_KEK_auth_len);
+  assert (le16_to_cpu (timestamp.year) == 2020);
 
   auth_data.name = (uint16_t *) DBX_NAME;
   auth_data.vendor = (uuid_t *) &SV_IMAGE_SECURITY_DATABASE_GUID;
@@ -92,14 +84,13 @@ main (int argc, char **argv)
    * 6b:bb:47:0b:52:59:f6:9e:02:07:94:39:93:ea:4b:25:8b:51:0e:df:c5:1f:
    * a5:bd:ba:df:9f:ba:92:4e:4b:82
    */
-  if (rc == SV_SUCCESS && hash[0] == 0x6b && hash[1] == 0xbb &&
-      hash[30] == 0x4b && hash[31] == 0x82)
-    printf ("Test case-5 : PASSED\n");
-  else
-    {
-      printf ("Test case-5 : FAILED with rc = 0x%x\n", rc);
-      return 0;
-    }
+  assert_rc (SV_SUCCESS);
+  assert (hash[0] == 0x6b);
+  assert (hash[1] == 0xbb);
+  assert (hash[30] == 0x4b);
+  assert (hash[31] == 0x82);
+
+  printf("PASS\n");
 
   return 0;
 }
